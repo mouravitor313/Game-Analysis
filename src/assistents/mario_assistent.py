1fd1db1a8810fd5194b560c1d6ede9d670b8a001
mouravitor313 <mouravitor0104@gmail.com>
2024-04-24 14:11:39 -0300

    feat Criação do assistente Mario e arquivos de dados Foram criados os arquivos mario_assistent.py e mario_prompts.py, além dos arquivo que irão receber os dados em csv.

---
+++ src/assistents/mario_assistent.py
@@ -0,0 +1,34 @@
+import openai
+import pandas as pd
+import io
+import os
+from prompts.mario_prompts import prompts
+
+API_KEY = os.getenv("API_KEY_OPENAI")
+openai.api_key = API_KEY
+
+dataframe = pd.read_csv('data/games_list.csv')
+
+def add_columns_and_complete_information(prompt,data):
+    completions = openai.chat.completions.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "user", "content": prompt+"\n\n"+data}
+        ],
+        temperature=0.3,
+    )
+    message_received_from_model = completions.choices[0].message.content
+    return message_received_from_model
+
+def export_csv_with_addition_information(message_received_from_model):
+    dataframe = pd.read_csv(io.StringIO(message_received_from_model))
+
+    if not os.path.isfile('data/games_data_output'):
+        dataframe.to_csv('data/games_data_output', index=False)
+        
+    else:
+        dataframe.to_csv('data/games_data_output', mode='a', header=False, index=False)
+
+
+
+
---
+++ src/prompts/mario_prompts.py
@@ -0,0 +1,2 @@
+prompts = {'complete_that_csv':"""I'm sending you this csv, and with it I need you to give me back another csv adding columns that I send you below and completing the information of the games already present in the csv that I sent you. The columns looked like this (I'm including the columns that are already in csv, to show you in what order the new columns should go in): name,genre,publisher,year,platform,estimate_time,completed,current_time
+           now the csv:"""}
