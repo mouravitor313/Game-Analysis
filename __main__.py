aae85c8e64ba93949964e8a3dbbb2d0520697968
mouravitor313 <mouravitor0104@gmail.com>
2024-04-28 18:50:13 -0300

    :recycle:refactor: Refatorado arquivo main Foram especificados os tipos de parâmetros e algumas variáveis para melhor leitura e entendimento do código.

--- __main__.py
+++ __main__.py
@@ -5,17 +5,17 @@ from src.prompts.mario_prompts import prompts
 class Main():
     
     dataframe = pd.read_csv('Game-Analisys/data/games_list.csv')
+    dataframe = dataframe.to_string()
 
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
     @classmethod
-    def main(cls):
+    def main(cls) -> None:
 
-        message_received_from_model = mario_assistant.add_columns_and_complete_information(prompts['complete_that_csv'], cls.dataframe)
+        message_received_from_model = '\n' + mario_assistant.add_columns_and_complete_information(prompts['complete_that_csv'], cls.dataframe)
 
-        mario_assistant.add_columns_and_complete_information(message_received_from_model)
-
+        mario_assistant.export_csv_with_addition_information(message_received_from_model, 'Game-Analisys/data/games_data_output.csv')
 
 if __name__ == '__main__':
     Main.main()
