b8ebdfa8b4ff15b82aa5947bfdf9b5b40f9323ed
mouravitor313 <mouravitor0104@gmail.com>
2024-04-29 18:21:26 -0300

    :recycle:refactor: Alterado nome dicionário prompts Nome dos dicionários de prompts dos dois modelos alterados para evitar possiveis erros durante a execução do arquivo main por terem nomes parecidos.

--- src/prompts/sonic_prompts.py
+++ src/prompts/sonic_prompts.py
@@ -1,4 +1,4 @@
 from typing import Dict
 
-prompts: Dict[str, str]= {'most_played_overall':"From this csv below, extract the information about the 10 games with the highest current_time overall",
-           'least_played_overall':"From this csv below, extract the information about the 10 games with the lowest complete_time overall"}
+prompts_sonic: Dict[str, str]= {'most_played_overall':"From this csv below, extract the information about the 10 games with the highest current_time overall",
+           'least_played_overall':"From this csv below, extract the information about the 10 games with the lowest current_time overall"}
