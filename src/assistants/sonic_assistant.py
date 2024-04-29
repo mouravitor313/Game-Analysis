142aed1fac611c3ab11a1ae7edc794626ca68edf
mouravitor313 <mouravitor0104@gmail.com>
2024-04-29 18:02:02 -0300

    :recycle:refactor Melhorado prompts do Sonic Prompts reescritos, corrigido um erro onde estava escrito complete_time no lugar de current_time, e removido a instrução para retornar a resposta no formato json.

--- src/assistants/sonic_assistant.py
+++ src/assistants/sonic_assistant.py
@@ -10,9 +10,9 @@ def collect_information_and_analyze(prompt: str) -> str:
     data = pd.read_csv('data/games_data_output')
     completions = openai.chat.completions.create(
         model="gpt-3.5-turbo-1106",
-        response_format={ "type": "json_object" },
+        # response_format={ "type": "json_object" },
         messages=[
-            {"role": "system", "content": "You are a helpful assistant which have the same personality as Sonic The Hedgehog and are designed to output JSON."},
+            {"role": "system", "content": "You are a helpful assistant which have the same personality as Sonic The Hedgehog."},
             {"role": "user", "content": prompt+"\n\n"+data}
         ],
         temperature=0.3,
