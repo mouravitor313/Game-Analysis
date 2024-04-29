a4637367094d6f3770b7a9517aee16882cbbfebc
mouravitor313 <mouravitor0104@gmail.com>
2024-04-29 19:24:47 -0300

    :recycle:refactor: Transformando Sonic em classe

--- src/assistants/sonic_assistant.py
+++ src/assistants/sonic_assistant.py
@@ -2,21 +2,24 @@ import openai
 import pandas as pd
 import os
 
-API_KEY = os.getenv("API_KEY_OPENAI")
-openai.api_key = API_KEY
+class Sonic():
+    API_KEY = os.getenv("API_KEY_OPENAI")
+    openai.api_key = API_KEY
 
-def collect_information_and_analyze(prompt: str) -> str:
+    @staticmethod
+    def collect_information_and_analyze(prompt: str, data: str) -> str:
 
-    data = pd.read_csv('data/games_data_output')
-    completions = openai.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        # response_format={ "type": "json_object" },
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant which have the same personality as Sonic The Hedgehog."},
-            {"role": "user", "content": prompt+"\n\n"+data}
-        ],
-        temperature=0.3,
-    )
-    message_received_from_model = completions.choices[0].message.content
-    return message_received_from_model
+        completions = openai.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            # response_format={ "type": "json_object" },
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant which have the same personality as Sonic The Hedgehog."},
+                {"role": "user", "content": prompt+"\n\n"+data}
+            ],
+            temperature=0.3,
+        )
+        message_received_from_model = completions.choices[0].message.content
+        return message_received_from_model
+
+
 
--- src/prompts/sonic_prompts.py
+++ src/prompts/sonic_prompts.py
@@ -1,4 +1,4 @@
 from typing import Dict
 
-prompts_sonic: Dict[str, str]= {'most_played_overall':"From this csv below, extract the information about the 10 games with the highest current_time overall",
+prompts_sonic: Dict[str, str]= {'most_played_overall':"From this csv below, extract the information about the 10 games with the highest current_time overall, even the csv it's seems incomplete",
            'least_played_overall':"From this csv below, extract the information about the 10 games with the lowest current_time overall"}
