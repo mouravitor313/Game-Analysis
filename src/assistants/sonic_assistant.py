329fdf77a62dc8684034fb167e0f4b8d8b249e73
mouravitor313 <mouravitor0104@gmail.com>
2024-05-03 15:39:51 -0300

    :sparkles:feat: Criada função de chat Sonic Só precisa agora adicionar um break aquele loop.

--- src/assistants/sonic_assistant.py
+++ src/assistants/sonic_assistant.py
@@ -20,6 +20,28 @@ class Sonic():
         )
         message_received_from_model = completions.choices[0].message.content
         return message_received_from_model
+    
+    @staticmethod
+    def chat_with_Sonic(data: str) -> str:
+
+        messages_default=[
+                {"role": "system", "content": "You are a helpful assistant which have the same personality as Sonic The Hedgehog."},
+                {"role": "user", "content": "Read that csv, I want to talk with you about that data"+"\n\n"+data}
+            ]
+        
+        while(True):
+            completions = openai.chat.completions.create(
+                model="gpt-3.5-turbo-1106",
+                messages=messages_default,
+                temperature=0.3,
+            )
+
+            message_received_from_model = completions.choices[0].message.content
+            messages_default.append({"role": "assistant", "content": message_received_from_model})
+            print("Sonic: ", message_received_from_model)
+            
+            user_message = input("You: ")
+            messages_default.append({"role": "user", "content": user_message})
 
 
 
