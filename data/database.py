a76d02b85cebbada0173dcdc26220a718e26a03e
mouravitor313 <mouravitor0104@gmail.com>
2024-06-12 11:17:02 -0300

    :sparkles:feat: Conexão com a database

---
+++ data/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base 
+
+URL_DATABASE = 'postgresql://postgres:442147@localhost:5432/gameassistant'
+
+engine = create_engine(URL_DATABASE)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
