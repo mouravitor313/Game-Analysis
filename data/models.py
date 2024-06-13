5cf7604bf6d7740fce97b1e85fb5b05bf4b72795
mouravitor313 <mouravitor0104@gmail.com>
2024-06-13 13:56:52 -0300

    :sparkles:feat:Criado arquivo Models Ciração das classes Users e Games para a criação das tabelas no banco de dados.

---
+++ data/models.py
@@ -0,0 +1,23 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    password_hash = Column(String)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Games(Base):
+    __tablename__ = 'choices'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    genre = Column(String)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
