5a44707e931c279731c629fb972059675fd0229c
mouravitor313 <mouravitor0104@gmail.com>
2024-06-18 18:10:05 -0300

    :sparkles: feat: Implementado criação de tokens Criação de tokens para autorização e autenticação do usuário utilizando AuthJWT. Também criada a função para coletar o id do usuário logado além da criação do token de acesso do usuário no arquivo user_routes.

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter, HTTPException, Depends
 from models import User
-from database import SessionLocal, get_db
-from sqlalchemy.orm import Session
+from database import db_depedency
 from schemas.models_bases import UserBase
+from fastapi_jwt_auth import AuthJWT
 
 router = APIRouter()
 
+def get_current_user(db: db_depedency, Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+    current_user = Authorize.get_jwt_subject()
+    return db.query(User).filter(User.username == current_user).first()
+
 @router.post("/register")
-async def register(user: UserBase, db: Session = Depends(get_db)):
+async def register(user: UserBase, db: db_depedency):
     user_query = db.query(User).filter(User.username == user.username).first()
     if user_query:
         raise HTTPException(status_code=400, detail="Username already registered")
@@ -18,8 +23,10 @@ async def register(user: UserBase, db: Session = Depends(get_db)):
     return {"message": "User created"}
 
 @router.post("/login")
-async def login(user: UserBase, db: Session = Depends(get_db)):
+async def login(user: UserBase, db: db_depedency, Authorize: AuthJWT = Depends()):
     user_query = db.query(User).filter(User.username == user.username).first()
     if not user_query or not user_query.check_password(user.password):
         raise HTTPException(status_code=400, detail="Invalid username or password")
-    return {"message": "Logged in"}
+    
+    access_token = Authorize.create_access_token(subject=user.username)
+    return {"access_token": access_token}
--- schemas/models_bases.py
+++ schemas/models_bases.py
@@ -4,7 +4,12 @@ class UserBase(BaseModel):
     username: str
     password: str
 
-class GamesBase(BaseModel):
+class GameBase(BaseModel):
     name: str
     genre: str
     completed: bool
+    user_id: int
+
+class TokenBase(BaseModel):
+    acess_token: str
+    token_type: str
