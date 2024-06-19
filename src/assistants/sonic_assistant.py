d300348f85ef8c1d713a72eaf129cf91ee5df34c
mouravitor313 <mouravitor0104@gmail.com>
2024-06-19 15:26:07 -0300

    :sparkles: feat: Modificado Sonic Assistant Removida a função para ler e analisar CSV, além de ser modificado o script do Sonic para se adequar mais com o novo estilo de entrada de dados.

--- src/assistants/sonic_assistant.py
+++ src/assistants/sonic_assistant.py
@@ -7,25 +7,10 @@ class Sonic():
     openai.api_key = API_KEY
 
     @staticmethod
-    def collect_information_and_analyze(prompt: str, data: str) -> str:
-
-        completions = openai.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            # response_format={ "type": "json_object" },
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant which have the same personality as Sonic The Hedgehog."},
-                {"role": "user", "content": prompt+"\n\n"+data}
-            ],
-            temperature=0.3,
-        )
-        message_received_from_model = completions.choices[0].message.content
-        return message_received_from_model
-    
-    @staticmethod
-    def chat_with_Sonic(data: str) -> str:
+    def chat_with_Sonic(prompt: str, data: str) -> str:
 
         messages_default=[
-                {"role": "system", "content": "You are a helpful assistant which have the same personality as Sonic The Hedgehog. Remember to talk like Sonic every time."},
+                {"role": "system", "content": prompt},
                 {"role": "user", "content": "Read that csv, I want to talk with you about that data"+"\n\n"+data}
             ]
         
